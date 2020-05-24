98981fab8b363ebd3400f5257f346bb089d0cf50
balajiofficial <balajiofficialmailme@gmail.com>
2020-05-24 12:58:09 -0700

    'new'

--- src/components/screens/DeviceClone.js
+++ src/components/screens/DeviceClone.js
@@ -8,8 +8,7 @@ import {
   ActivityIndicator,
   StyleSheet,
   Clipboard,
-  Text,
-  TouchableWithoutFeedback 
+  Text
 } from "react-native";
 import DeviceComponent from "../devices/DeviceComponent";
 import React from "react";
@@ -49,114 +48,7 @@ class DevicesScreen extends React.Component<Props> {
   constructor(props) {
     super(props);
     new GoogleAnalyticsTracker(ID).trackScreenView("Main");
-    state={
-      group_image:''
-    }
   }
-
-  // require('../../')
- DATA = [
-    {
-      id: "1",
-      title: "LIVING ROOM",
-image:require('../../assets/group/bg.png'),
-group:'living_room',
-top_icon_bg_color:'green'
-    },
-    {
-      id: "2",
-      title: "BED ROOM",
-image:require('../../assets/group/bg.png'),
-group:'bed_room',
-top_icon_bg_color:'lightblue'
-
-    },
-        {
-      id: "2",
-      title: "KIDS ROOM",
-image:require('../../assets/group/bg.png'),
-group:'kids_room',
-top_icon_bg_color:'yellow'
-
-    },
-
-    
-    {
-      id: "3",
-      title: "KICHEN",
-image:require('../../assets/group/bg.png'),
-group:'kitchen',
-top_icon_bg_color:'red'
-    },
-    {
-      id: "4",
-      title: "BOILER ROOM",
-image:require('../../assets/group/bg.png'),
-group:'boilder_room',
-top_icon_bg_color:'orange'
-    },
-    {
-      id: "5",
-      title: "GARDEN",
-image:require('../../assets/group/bg.png'),
-group:'garden',
-top_icon_bg_color:'darkgreen'
-    },
-//     {
-//       id: "6",
-//       title: "SABOR MORENO",
-// image:require('../../assets/group/bg.png')
-//     },
-//     {
-//       id: "7",
-//       title: "0 MESTRE PUB",
-// image:require('../../assets/group/bg.png')
-//     },
-//     {
-//       id: "8",
-//       title: "GRILL 54 CHEF",
-// image:require('../../assets/group/bg.png')
-//     }
-  ];
-  
-
-
-  switch_group=(condition)=>{
-
-    switch (condition) {
-      case 'living_room':
-
-        return require('../../assets/group/living_room.png');
-      case 'bed_room':
-
-      return require('../../assets/group/bedroom.png');
-      
-      case 'kids_room':
-
-       return require('../../assets/group/kids_room.png');
-
-      case 'kitchen':
-
-      return require('../../assets/group/kitchen.jpg');
-      case 'boiler_room':
-
-      return require('../../assets/group/bg.png');
-      case 'garden':
-
-        return require('../../assets/group/garden.png');
-  
-          
-
-
-      default:
-        return require('../../assets/group/bg.png');
-
-    }
-
-
-  }
-
-
 
   renderSeparator = () => {
     return (
@@ -193,41 +85,6 @@ top_icon_bg_color:'darkgreen'
     }
   };
 
-//newly added function 
-  group_iot = ({ item }) => (
-    <View style={{ flex: 1, marginHorizontal: 5, marginBottom: 20,shadowColor: 'red',
-    shadowOffset: { width: 2, height: 2 },
-    shadowOpacity: 0.9,
-    shadowRadius: 10,  
-    elevation: 15 }}>
-      <Image
-        style={{ width: "100%", height: 180 }}
-        // source={{ uri: item.image }}
-        // source={item.image}
-        source={this.switch_group(item.group)}
-      />
-
-<View   style={{ justifyContent:'center',alignItems:'center', position:"absolute",borderRadius :50,backgroundColor:item.top_icon_bg_color, top:5,left:5, width: 60, height: 60,overflow:"hidden", }}>
-<Image
-        style={{  width: 30, height: 30 ,zIndex: 500 }}
-        // source={{ uri: item.image }}
-        source={this.switch_group(item.group)}
-      />
-    
-</View>
-<Text style={{ position:"absolute",fontSize:12,fontWeight: '900',   textAlign: "center", marginTop: 8,bottom:10,left:10 }}>{item.title}</Text>
-<View   style={{ justifyContent:'center',alignItems:'center',fontSize:18,fontWeight: "bold", position:"absolute",borderRadius :50,backgroundColor:'rgba(211, 211, 211, 0.4)', bottom:10,right:5, width: 45, height: 45,shadowColor: 'red',
-    shadowOffset: { width: 2, height: 2 },
-    shadowOpacity: 0.9,
-    shadowRadius: 10,  
-    elevation: 15 }}>  
-<Text style={{fontSize:20,zIndex: 500,fontWeight:'900'}}>></Text>
-</View>
-    </View>
-  );
-
-
-
   renderContent() {
     const {
       devices,
@@ -239,17 +96,7 @@ top_icon_bg_color:'darkgreen'
     return (
       <View style={{ flex: 1 }}>
          <Text>devices</Text>
-         <FlatList
-          data={this.DATA}
-          renderItem={this.group_iot}
-          keyExtractor={item => item.id}
-          numColumns={2}
-          style={{ flex: 1 }}
-          contentContainerStyle={{ paddingVertical: 20 }}
-        />
-
-
-        {/* {devices.length ? (
+        {devices.length ? (
           <FlatList
             data={devices}
             keyExtractor={item => item.id}
@@ -275,7 +122,7 @@ top_icon_bg_color:'darkgreen'
             <H1Text>Ooops!</H1Text>
             <H2Text>You could add a device...</H2Text>
           </View>
-        )} */}
+        )}
 
         {!isUserDevices && (
           <ActionButton buttonColor="rgba(231,76,60,1)">
